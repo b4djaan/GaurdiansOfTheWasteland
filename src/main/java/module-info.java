a32f602ec071d2e2f4c6b962513ca86f48114f86
b4djaan <daanbroek05@gmail.com>
2024-12-05 11:35:17 +0100

    adiya's deel
    hoop dat alles in de toekomst goed gaat

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
-module com.example.javafxmockup {
+module com.example.mockup {
     requires javafx.controls;
     requires javafx.fxml;
 
     requires com.almasb.fxgl.all;
 
-    opens com.example.javafxmockup to javafx.fxml;
-    exports com.example.javafxmockup;
+    opens com.example.mockup to javafx.fxml;
+    exports com.example.mockup;
 }
